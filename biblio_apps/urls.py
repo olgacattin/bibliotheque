a8f75fbe7dcab06c56ad09e23df582b0c6d2bd54
Olga <olga@ativ-bcis>
2014-08-28 15:12:28 +0200

    fix urls files.

--- biblio_apps/urls.py
+++ biblio_apps/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from biblio_apps import views
 
 urlpatterns = patterns('', 
-    url(r'^$', views.index, name='index'),
-    url(r'^$', views.livres_list, name='livres_list'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^livres_list/$', views.livres_list, name='livres_list'),
 )
